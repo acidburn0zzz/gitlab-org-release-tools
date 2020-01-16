44a3c8ea15f783489d5bbff6ddf21b4ea217b310
John T Skarbek <jskarbek@gitlab.com>
2020-01-16 15:23:54 -0500

    redo passing_build tests

--- spec/lib/release_tools/passing_build_spec.rb
+++ spec/lib/release_tools/passing_build_spec.rb
@@ -77,78 +77,34 @@ describe ReleaseTools::PassingBuild do
 
       context 'with component changes' do
         let(:cng_project) { ReleaseTools::Project::CNGImage }
-        let(:cng_variables) do
-          <<EOS
----
-variables:
-  GITLAB_ELASTICSEARCH_INDEXER_VERSION: v1.5.0
-  GITLAB_VERSION: v12.6.3
-  GITLAB_REF_SLUG: v12.6.3
-  GITLAB_ASSETS_TAG: v12.6.3
-  GITLAB_EXPORTER_VERSION: 5.1.0
-  GITLAB_SHELL_VERSION: v10.3.0
-  GITLAB_WORKHORSE_VERSION: v8.18.0
-  GITLAB_CONTAINER_REGISTRY_VERSION: v2.7.6-gitlab
-  GITALY_VERSION: master
-  GIT_VERSION: 2.24.1
-  GO_VERSION: 1.12.13
-  KUBECTL_VERSION: 1.13.12
-  PG_VERSION: '10.9'
-  MAILROOM_VERSION: 0.10.0
-  ALPINE_VERSION: '3.10'
-  CFSSL_VERSION: '1.2'
-  DOCKER_DRIVER: overlay2
-  DOCKER_HOST: tcp://docker:2375
-  DOCKER_TLS_CERTDIR: ''
-  ASSETS_IMAGE_PREFIX: gitlab-assets
-  ASSETS_IMAGE_REGISTRY_PREFIX: registry.gitlab.com/gitlab-org
-  GITLAB_NAMESPACE: gitlab-org
-  CE_PROJECT: gitlab-foss
-  EE_PROJECT: gitlab
-  COMPILE_ASSETS: 'false'
-  S3CMD_VERSION: 2.0.1
-  PYTHON_VERSION: 3.7.3
-  GITALY_SERVER_VERSION: v1.77.1
-EOS
-        end
+        let(:omnibus_project) { ReleaseTools::Project::OmnibusGitlab }
 
         before do
           allow(fake_client).to receive(:project_path)
             .with(cng_project)
             .and_return(cng_project.path)
 
-          allow(fake_client).to receive(:file_contents)
-            .with(cng_project.path, "/ci_files/variables.yml", '11-10-auto-deploy-1234')
-            .and_return(cng_variables)
           allow(ReleaseTools::ComponentVersions)
-            .to receive(:omnibus_version_changes?).and_return(true)
-
-          allow(ReleaseTools::ComponentVersions)
-            .to receive(:cng_version_changes?).and_return(true)
-        end
-
-        it 'updates Omnibus versions and tags' do
-          expect(ReleaseTools::ComponentVersions)
             .to receive(:update_omnibus).with('11-10-auto-deploy-1234', version_map)
             .and_return(fake_commit)
 
-          expect(service).to receive(:tag_project).with(ReleaseTools::Project::OmnibusGitlab, fake_commit)
-
-          without_dry_run do
-            service.trigger_build
-          end
-        end
-
-        it 'updates CNG versions and tags' do
-          expect(ReleaseTools::ComponentVersions)
+          allow(ReleaseTools::ComponentVersions)
             .to receive(:update_cng).with('11-10-auto-deploy-1234', version_map)
             .and_return(fake_commit)
 
-          expect(service).to receive(:tag_project).with(ReleaseTools::Project::CNGImage, fake_commit)
+          allow(ReleaseTools::ComponentVersions)
+            .to receive(:omnibus_version_changes?).and_return(false)
 
-          without_dry_run do
-            service.trigger_build
-          end
+          allow(ReleaseTools::ComponentVersions)
+            .to receive(:cng_version_changes?).and_return(false)
+        end
+
+        it 'updates Omnibus versions and tags' do
+          expect(service).to receive(:tag_project).with(omnibus_project, fake_commit)
+        end
+
+        it 'updates CNG versions and tags' do
+          expect(service).to receive(:tag_project).with(cng_project, fake_commit)
         end
       end
 
