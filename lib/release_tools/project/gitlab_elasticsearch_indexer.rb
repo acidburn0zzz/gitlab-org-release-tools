af9a02354502204f2cd8cdeaeceb978c220ad2ac
Robert Speicher <rspeicher@gmail.com>
2020-04-03 16:56:42 -0500

    GitlabElasticsearchIndexer security path is dev

--- lib/release_tools/project/gitlab_elasticsearch_indexer.rb
+++ lib/release_tools/project/gitlab_elasticsearch_indexer.rb
@@ -5,7 +5,8 @@ module ReleaseTools
     class GitlabElasticsearchIndexer < BaseProject
       REMOTES = {
         canonical: 'git@gitlab.com:gitlab-org/gitlab-elasticsearch-indexer.git',
-        dev: 'git@dev.gitlab.org:gitlab/gitlab-elasticsearch-indexer.git'
+        dev: 'git@dev.gitlab.org:gitlab/gitlab-elasticsearch-indexer.git',
+        security: 'git@dev.gitlab.org:gitlab/gitlab-elasticsearch-indexer.git'
       }.freeze
 
       def self.version_file
