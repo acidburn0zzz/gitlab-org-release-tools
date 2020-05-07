505246318bd1616eb6d0391bb90b43090aa4ebfd
Mayra Cabrera <mcabrera@gitlab.com>
2020-05-07 10:49:31 -0500

    Fixes cherry-picking failure on security service
    Previously we used the merge requests associated to an issue, but none
    of those had the merge_commit_sha information generating an invalid URL
    on the POST request.
    
    This commit sends the merge result instead that contains a
    merge_commit_sha.
    
    CherryPicker service implementation was also simplified because of it.
    
    Related to https://gitlab.com/gitlab-org/release-tools/-/issues/431
    and to https://gitlab.com/gitlab-org/release-tools/-/issues/429

--- lib/release_tools/security/cherry_picker.rb
+++ lib/release_tools/security/cherry_picker.rb
@@ -7,64 +7,51 @@ module ReleaseTools
     class CherryPicker
       include ::SemanticLogger::Loggable
 
-      attr_reader :merge_requests
-
-      def initialize(client, merge_requests)
-        @merge_requests = merge_requests
+      def initialize(client, merge_request)
         @client = client
+        @merge_request = merge_request
         @target = ReleaseTools::AutoDeployBranch.current_name
       end
 
       def execute
         return unless Feature.enabled?(:security_cherry_picker)
 
-        filter_by_target!
-        filter_by_branch!
-
-        @merge_requests.each do |merge_request|
-          @client.cherry_pick_commit(
-            merge_request.project_id,
-            merge_request.merge_commit_sha,
-            @target
-          )
-
-          logger.info(
-            'Cherry-picked security merge request to auto-deploy',
-            project: merge_request.project_id,
-            ref: merge_request.merge_commit_sha,
-            target: @target,
-            merge_request: merge_request.web_url
-          )
-        rescue ::Gitlab::Error::Error => ex
+        if @merge_request.merge_commit_sha.present?
+          cherry_pick_into_auto_deploy
+        else
           logger.fatal(
-            'Failed security cherry-pick to auto-deploy',
-            project: merge_request.project_id,
-            ref: merge_request.merge_commit_sha,
-            target: @target,
-            merge_request: merge_request.web_url,
-            error: ex.message
+            'Security merge request does not have a merge commit sha',
+            merge_request: @merge_request.web_url,
+            target: @target
           )
         end
       end
 
       private
 
-      # Remove merge requests not targeting `master`
-      def filter_by_target!
-        @merge_requests.select! { |mr| mr.target_branch == 'master' }
-      end
+      def cherry_pick_into_auto_deploy
+        @client.cherry_pick_commit(
+          @merge_request.project_id,
+          @merge_request.merge_commit_sha,
+          @target
+        )
 
-      # Remove merge requests belonging to a project with no auto-deploy branch
-      def filter_by_branch!
-        projects = @merge_requests.collect(&:project_id).uniq
-
-        projects.select! do |project_id|
-          @client.branch(project_id, @target)
-        rescue ::Gitlab::Error::NotFound
-          false
-        end
-
-        @merge_requests.select! { |mr| projects.include?(mr.project_id) }
+        logger.info(
+          'Cherry-picked security merge request to auto-deploy',
+          project: @merge_request.project_id,
+          ref: @merge_request.merge_commit_sha,
+          target: @target,
+          merge_request: @merge_request.web_url
+        )
+      rescue ::Gitlab::Error::Error => ex
+        logger.fatal(
+          'Failed security cherry-pick to auto-deploy',
+          project: @merge_request.project_id,
+          ref: @merge_request.merge_commit_sha,
+          target: @target,
+          merge_request: @merge_request.web_url,
+          error: ex.message
+        )
       end
     end
   end
--- spec/lib/release_tools/security/cherry_picker_spec.rb
+++ spec/lib/release_tools/security/cherry_picker_spec.rb
@@ -3,64 +3,84 @@
 require 'spec_helper'
 
 describe ReleaseTools::Security::CherryPicker do
-  let(:client) { spy }
-  let(:merge_result) do
-    [
-      double(project_id: 1, merge_commit_sha: 'a', target_branch: 'master').as_null_object,
-      double(project_id: 1, merge_commit_sha: 'b', target_branch: '12-9-stable-ee').as_null_object,
-      double(project_id: 2, merge_commit_sha: 'c', target_branch: '12-9-stable-ee').as_null_object,
-      double(project_id: 3, merge_commit_sha: 'd', target_branch: 'master').as_null_object,
-      double(project_id: 4, merge_commit_sha: 'e', target_branch: 'master').as_null_object
-    ]
+  let(:client) { double(:client) }
+
+  let(:web_url) do
+    'https://gitlab.com/gitlab-org/security/gitlab/-/merge_requests/1'
   end
 
+  let(:merge_request) do
+    double(
+      :merge_request,
+      target_branch: 'master',
+      merge_commit_sha: 'aaa',
+      project_id: 1,
+      web_url: web_url
+    )
+  end
+
+  subject(:picker) { described_class.new(client, merge_request) }
+
   before do
-    enable_feature(:security_cherry_picker)
-
-    stub_const('ReleaseTools::Security::Client', client)
-
     allow(ReleaseTools::AutoDeployBranch).to receive(:current_name)
       .and_return('X-Y-auto-deploy-YYYYMMDD')
 
-    # Odd-numbered projects have no auto-deploy branch
-    allow(client).to receive(:branch) do |project_id, _|
-      if project_id.odd? # rubocop:disable Style/GuardClause
-        raise gitlab_error(:NotFound, code: 404)
-      else
-        double('MergeRequest').as_null_object
-      end
-    end
+    allow(client)
+      .to receive(:cherry_pick_commit)
   end
 
   describe '#execute' do
-    it 'cherry-picks merge requests targeting `master` in projects with auto-deploy branches', :aggregate_failures do
-      picker = described_class.new(client, merge_result)
-
-      picks, skipped = merge_result.partition { |mr| mr.project_id.even? && mr.target_branch == 'master' }
-
-      picks.each do |mr|
-        expect(client).to receive(:cherry_pick_commit).with(
-          mr.project_id,
-          mr.merge_commit_sha,
-          ::ReleaseTools::AutoDeployBranch.current_name
-        )
+    context 'when the feature is enabled' do
+      before do
+        enable_feature(:security_cherry_picker)
       end
 
-      skipped.each do |mr|
-        expect(client).not_to receive(:cherry_pick_commit)
-          .with(anything, mr.merge_commit_sha, anything)
+      context 'when the merge request has a merge commit sha' do
+        it 'cherry picks the commit into auto-deploy' do
+          expect(client).to receive(:cherry_pick_commit).with(
+            merge_request.project_id,
+            merge_request.merge_commit_sha,
+            ::ReleaseTools::AutoDeployBranch.current_name
+          )
+
+          picker.execute
+        end
+
+        it 'handles failed cherry-picks' do
+          expect(client).to receive(:cherry_pick_commit)
+            .and_raise(gitlab_error(:BadRequest, code: 400))
+
+          expect { picker.execute }.not_to raise_error
+        end
       end
 
-      picker.execute
+      context 'when the merge request does not have a merge commit sha' do
+        let(:merge_request) do
+          double(
+            :merge_request,
+            target_branch: 'master',
+            merge_commit_sha: '',
+            project_id: 1,
+            web_url: 'https://gitlab.com/gitlab-org/security/gitlab/-/merge_requests/1'
+          )
+        end
+
+        it 'skips the cherry pick' do
+          expect(client).not_to receive(:cherry_pick_commit)
+
+          picker.execute
+        end
+      end
     end
 
-    it 'handles failed cherry-picks' do
-      expect(client).to receive(:cherry_pick_commit)
-        .and_raise(gitlab_error(:BadRequest, code: 400))
+    context 'when the feature is disabled' do
+      it 'does not executes the cherry pick' do
+        disable_feature(:security_cherry_picker)
 
-      picker = described_class.new(client, merge_result)
+        expect(client).not_to receive(:cherry_pick_commit)
 
-      expect { picker.execute }.not_to raise_error
+        picker.execute
+      end
     end
   end
 end
